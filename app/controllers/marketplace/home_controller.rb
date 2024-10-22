dc6ddd897889953d67ca1ef2cf400d7e02c3c3c4
bablu <bk147411@gmail.com>
2024-10-22 14:17:19 +0530

    Recent Creators

--- app/controllers/marketplace/home_controller.rb
+++ app/controllers/marketplace/home_controller.rb
@@ -8,14 +8,13 @@ module Marketplace
       @languag_count = Language.count 
       @languages = Language.order(:name).limit(20)
       @categories = Category.order(:name).limit(20)
-      @creators = User.order(total_earning: :desc).limit(20)
+      @creators = User.order(created_at: :desc).limit(20)
       @recent_products = Product.with_attached_covers.published.includes([:languages]).recent.exclude_purchased(current_user).first(15)
       @popular_products = Product.with_attached_covers.includes([:languages]).ordered_by_purchase_count.first(15)
       @free_products = Product.with_attached_covers.includes([:languages]).where("price_cents <= 0").order(created_at: :desc).first(15)
       @premium_products = Product.with_attached_covers.includes([:languages]).where("price_cents > 0").order(created_at: :desc).first(15)
       @featured_products = Product.with_attached_covers.includes([:languages]).where(featured: true)
 
-      @creators = recently_subscribed()
     end
     def resources
     
