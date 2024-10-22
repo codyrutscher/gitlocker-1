b82adf31a37f27d499e0e604462262d28fcd9222
bablu <bk147411@gmail.com>
2024-10-22 20:10:53 +0530

    More From this Creator

--- app/models/product.rb
+++ app/models/product.rb
@@ -189,4 +189,9 @@ class Product < ApplicationRecord
        .distinct
        .limit(5)
   end
+
+  def more_from_this_creators
+    user = self.user 
+    products = user.products.order(created_at: :desc).limit(5)
+  end
 end
