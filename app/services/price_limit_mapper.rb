f0a5315de60cd835a0faf47a00c6428639fd8cd3
Cody Rutscher <44309879+codyrutscher@users.noreply.github.com>
2024-12-14 08:12:31 -0600

    Update price_limit_mapper.rb

--- app/services/price_limit_mapper.rb
+++ app/services/price_limit_mapper.rb
@@ -11,4 +11,17 @@ module PriceLimitMapper
   def self.get_product_limit(price_id)
     PRICE_LIMITS[price_id] || 10
   end
+  
+  def self.find_plan_name(price_id)
+    plans = {
+      "FREELANCER" => "price_1QP583SGQ7u76ltnMrfVcDwa",
+      "STARTUP" => "price_1QP58USGQ7u76ltnoQpSCcbq",
+      "ENTERPRISE" => "price_1QP58uSGQ7u76ltn8xV0JC3E",
+      "FREELANCER_YEAR" => "price_1QTGUxSGQ7u76ltndlBm2vt8",
+      "STARTUP_YEAR" => "price_1QTGVkSGQ7u76ltn5kG22ySi",
+      "ENTERPRISE_YEAR" => "price_1QTGWeSGQ7u76ltnXPgnQANw"
+    }
+    plan_name = plans.key(price_id)
+    plan_name || "Unknown Plan"
+  end
 end
