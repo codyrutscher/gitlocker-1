ff7c0788a7c8786acec61ce5df1670385440d400
Cody Rutscher <44309879+codyrutscher@users.noreply.github.com>
2024-12-17 06:51:45 -0600

    Update categories_controller.rb

--- app/controllers/marketplace/categories_controller.rb
+++ app/controllers/marketplace/categories_controller.rb
@@ -2,7 +2,7 @@ module Marketplace
 class CategoriesController < ApplicationController
   def show
     @category = Category.friendly.find(params[:slug])
-    @products = apply_filters_and_sort(@category&.products)&.page(params[:page])&.per(140)
+    @products = apply_filters_and_sort(@category&.products)&.page(params[:page])&.per(100)
   end
 
   private
