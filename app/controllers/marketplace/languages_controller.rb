070b9eeac56e9ebe27110b5522ab0b7c3c7c32af
Cody Rutscher <44309879+codyrutscher@users.noreply.github.com>
2024-12-28 10:11:50 -0600

    Update languages_controller.rb

--- app/controllers/marketplace/languages_controller.rb
+++ app/controllers/marketplace/languages_controller.rb
@@ -3,7 +3,7 @@ module Marketplace
 class LanguagesController < ApplicationController
   def show
     @language = Language.friendly.find(params[:slug])
-    params[:sort_by] ||= "most_recent"
+    params[:sort_by] ||= "most_recent" unless params[:sort_by].present?
     @products = apply_filters_and_sort(@language&.products).includes([:languages])&.page(params[:page])&.per(100)
   end
 
