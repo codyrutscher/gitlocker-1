83d807001be87584501c440f204ef3b6bb736473
Cody Rutscher <44309879+codyrutscher@users.noreply.github.com>
2024-12-28 10:10:12 -0600

    Update categories_controller.rb

--- app/controllers/marketplace/categories_controller.rb
+++ app/controllers/marketplace/categories_controller.rb
@@ -2,6 +2,7 @@ module Marketplace
 class CategoriesController < ApplicationController
   def show
     @category = Category.friendly.find(params[:slug])
+    params[:sort_by] ||= "most_recent"
     @products = apply_filters_and_sort(@category&.products)&.page(params[:page])&.per(100)
   end
 
