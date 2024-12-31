b79fcaa1d3e8ed1139a8c7075e6d15c168b25a7c
Cody Rutscher <44309879+codyrutscher@users.noreply.github.com>
2024-12-31 16:45:20 -0600

    Update home_controller.rb

--- app/controllers/marketplace/home_controller.rb
+++ app/controllers/marketplace/home_controller.rb
@@ -15,7 +15,7 @@ module Marketplace
       @free_products = Product.with_attached_covers.includes([:languages]).where("price_cents <= 0").exclude_purchased(current_user).order(created_at: :desc).first(10)
       @premium_products = Product.with_attached_covers.includes([:languages]).where("price_cents > 0").exclude_purchased(current_user).order(created_at: :desc).first(10)
       @featured_products = Product.with_attached_covers.includes([:languages]).where(featured: true).exclude_purchased(current_user).first(10)
-      @blogs = Blog.includes([:image_attachment], image_attachment: :blob).first(10)
+      @blogs = Blog.includes([:image_attachment], image_attachment: :blob).first(20)
 
     end
     def resources
