ed84194c1d4122f8fd51b3fe0bb920e743cd8d4e
zhamza372 <hamza.zahid@7vals.com>
2025-02-19 21:35:42 +0500

    image loading issue and performance fixes

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,10 +1,10 @@
 class BlogsController < ApplicationController
   def index
-    @blogs = Blog.includes([:image_attachment], image_attachment: :blob).page(params[:page]).per(60)
+    @blogs = Blog.includes(image_attachment: :blob).page(params[:page]).per(60)
   end
 
   def show
-    @blog = Blog.includes([:image_attachment], image_attachment: :blob).friendly.find(params[:slug])
+    @blog = Blog.friendly.find(params[:slug])
   end
 
   def export_data
@@ -12,7 +12,7 @@ class BlogsController < ApplicationController
       csv << Blog.column_names + ["image_filename"]
     
       Blog.find_each do |blog|
-        image_filename = blog.image.attached? ? blog.image.filename.to_s : nil
+        image_filename = blog.image.attached? ? rails_blob_url(blog.image, only_path: false) : nil
         csv << blog.attributes.values + [image_filename]
       end
     end
