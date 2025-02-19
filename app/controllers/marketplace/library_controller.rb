1d5bf99d914d59f927681e72975a775936b0b0d7
zhamza372 <hamza.zahid@7vals.com>
2025-02-19 23:33:46 +0500

    images on product marketplace show page handled and performance issue resolved

--- app/controllers/marketplace/library_controller.rb
+++ app/controllers/marketplace/library_controller.rb
@@ -10,7 +10,7 @@ class LibraryController < ApplicationController
       product.update(featured: true)
       product.featured_payment_intent.update(status: "paid", session_id: session_object.id)
     end 
-    @product = Product.includes(:languages, :categories).friendly.find(params[:id])
+    @product = Product.includes(:languages, :categories).with_attached_covers.friendly.find(params[:id])
     @related_products = @product.related_products
     @more_from_this_creators = @product.more_from_this_creators
     @reviews = @product.reviews.includes(:user).page(params[:page]).per(5)
