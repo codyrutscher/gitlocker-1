4fa06fe3c52ce7b5d8e92f1518a0558c31183aad
Cody Rutscher <44309879+codyrutscher@users.noreply.github.com>
2025-03-09 06:07:14 -0500

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -149,6 +149,9 @@ class ProductsController < ApplicationController
   def teamrepositories
   end
 
+  def templatesindex
+  end
+
   def create_from_gitlab
     repo_ids = params[:repo_ids]
     failed_repos = []
